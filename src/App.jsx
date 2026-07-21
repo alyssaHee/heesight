63cce2a52c093e737d209647870dc8fcc8e8bbb3
Alyssa Hee <alyssahee43@gmail.com>
2026-07-21 15:05:20 -0500

    Add loading screen

--- src/App.jsx
+++ src/App.jsx
@@ -9,6 +9,7 @@ function App() {
 
   return (
     <div className="app">
+      <Overlay />
       <Canvas
         style={{
           background:
--- src/Overlay.jsx
+++ src/Overlay.jsx
@@ -1,20 +1,36 @@
 import { useState } from 'react'
+import { useProgress } from '@react-three/drei'
 import './overlay.css'
 
 function Overlay({ loaded }) {
+    const { progress } = useProgress()
+    const [isRevealed, setIsRevealed] = useState(false)
+    const [isAnimationFinished, setIsAnimationFinished] = useState(false)
+
+    const handleReveal = () => {
+        setIsRevealed(true)
+    }
+
+    const handleAnimationFinished = () => {
+        setIsAnimationFinished(true)
+    }
+
+    if (isAnimationFinished) {
+        return null
+    }
+
     return (
         <div className="overlay">
-            <button className="overlay-button"
-                disabled={!loaded}
-                onClick={handleClick}>Enter</button>
-            <p>Use left click and drag to navigate!</p>
+            <div className={`overlay-top-half ${isRevealed ? 'revealed' : ''}`}
+                onTransitionEnd={handleAnimationFinished}></div>
+            <div className={`overlay-bottom-half ${isRevealed ? 'revealed' : ''}`}></div>
+            <div className="overlay-info-container">
+                <p className={`loading-text ${isRevealed ? 'revealed' : ''}`}>Use left click and drag to navigate!</p>
+                {progress < 100 ? (null) : !isRevealed ? (<button onClick={handleReveal} className="overlay-button">Enter</button>) : null}
+            </div>
         </div>
     )
 }
 
-function handleClick() {
-    const overlay = document.querySelector('.overlay')
-    overlay.style.display = 'none'
-}
 
 export default Overlay
