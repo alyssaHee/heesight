9d8c6e2d0d157796d6606c3b7e8949850c5f2fcf
Alyssa Hee <alyssahee43@gmail.com>
2026-08-02 23:18:10 -0500

    Update model texture

--- src/Scope/Scope.jsx
+++ src/Scope/Scope.jsx
@@ -52,7 +52,7 @@ function Scope({ onLoaded, onLockedChange }) {
         loader.setDRACOLoader(dracoLoader)
     })
 
-    const texture = useLoader(THREE.TextureLoader, '/textures/Bake6.webp')
+    const texture = useLoader(THREE.TextureLoader, '/textures/Bake6july.webp')
 
 
     useEffect(() => {
@@ -375,8 +375,8 @@ function Scope({ onLoaded, onLockedChange }) {
                                 <iframe
                                     className="screen-iframe"
                                     scrolling="no"
-                                    src="https://scope-screen.vercel.app/"
-                                    //src="http://localhost:5174/"
+                                    //src="https://scope-screen.vercel.app/"
+                                    src="http://localhost:5174/"
                                     style={{ pointerEvents: locked && !unlocking ? 'auto' : 'none', touchAction: 'manipulation' }}
                                 />
                             </Html>
