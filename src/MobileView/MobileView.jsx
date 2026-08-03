21156391b8b236be0a6989f48b034a31ed037cdb
Alyssa Hee <alyssahee43@gmail.com>
2026-08-03 16:15:12 -0500

    Update rotate image

--- src/MobileView/MobileView.jsx
+++ src/MobileView/MobileView.jsx
@@ -1,6 +1,6 @@
 import { useEffect, useMemo, useRef, useState } from 'react'
 import './mobileview.css'
-import rotateIcon from '../assets/rotate.png'
+import rotateIcon from '../assets/rotate1.png'
 
 function MobileView() {
     useEffect(() => {
